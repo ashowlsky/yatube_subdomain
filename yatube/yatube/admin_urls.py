c15782caec647936f252e41bd54df5a62e9922d6
Александр Шаульский <ashowlsky@yandex.ru>
2020-02-16 01:01:55 +0300

    fixed logout issue

--- yatube/yatube/admin_urls.py
+++ yatube/yatube/admin_urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from posts import views
 
 urlpatterns = [
     path("", admin.site.urls, name='wildcard'),
+    path("logout/", views.index, name='index'),
 ]
